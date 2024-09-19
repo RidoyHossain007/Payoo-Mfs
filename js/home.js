30fbc538fce2d5c99e67be13bc5c14b0fd24f721
Ridoy Hossain <hossainridoy0007@gmail.com>
2024-09-19 16:06:11 +0600

    incomplete add money handler

---
+++ js/home.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+     event.preventDefault();
+     const addMoneyInput=document.getElementById('input-add-money').value;
+     console.log(addMoneyInput);
+     const pin=document.getElementById('input-pin').value;
+     console.log(addMoneyInput);
+     
+
+     
+})
