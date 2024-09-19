d26ede5ad6bee6acb366fd1572751cf292f057a1
Ridoy Hossain <hossainridoy0007@gmail.com>
2024-09-19 14:14:15 +0600

    login page compleated

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
 event.preventDefault();
-console.log('button clicked');
-
+const phoneNumber= document.getElementById('phone-number').value;
+     const pinNumber=document.getElementById('pin-number').value;
+    if (phoneNumber==='1234'&& pinNumber==='1234') {
+     console.log('You are logged in');
+     window.location.href='/home.html';
      
-})
+    } 
+    else(
+     alert ('Invalid PIN of Phone Number')
+    )
+     
+}
+)
